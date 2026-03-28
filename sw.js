5d73654ee64c9d482d360c6ad77d328e0107c928
BHU Deploy <deploy@bhu.local>
2026-03-28 17:13:02 +0000

    Add PWA support - installable on iOS/Android/Desktop

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'bhu-discharge-v1';
+const URLS = [
+  '/bhu-discharge/login.html',
+  '/bhu-discharge/discharge-tracker.html',
+  '/bhu-discharge/admin.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
